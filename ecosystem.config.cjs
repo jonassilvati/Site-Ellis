d6b3ffe732da812b9161ca7434732db44dd37fce
Jonas da Costa Silva <jonassilvati@gmail.com>
2026-06-30 15:53:30 -0300

    feat: adicionar configuração do ecosystem e script de inicialização

---
+++ ecosystem.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: 'ellis',
+      script: '.output/server/index.mjs',
+      env_file: '.env',
+      instances: 1,
+      exec_mode: 'fork',
+      restart_delay: 3003,
+      max_restarts: 10,
+    },
+  ],
+}
