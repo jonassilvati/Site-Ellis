842f52c702482ee36b982db599f352231d4be12d
Jonas da Costa Silva <jonassilvati@gmail.com>
2026-06-30 15:58:38 -0300

    fix: corrigir atraso de reinício e ajustar configuração da porta

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,8 +6,11 @@ module.exports = {
       env_file: '.env',
       instances: 1,
       exec_mode: 'fork',
-      restart_delay: 3003,
+      restart_delay: 3000,
       max_restarts: 10,
+      env: {
+        PORT: 3003,
+      },
     },
   ],
 }
